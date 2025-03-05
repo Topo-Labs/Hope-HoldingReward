50386b06e556a7e2b25bd8e890d00800aab4a8c5
stevekeol <stevekeol.x@gmail.com>
2025-03-05 20:22:41 +0800

    :necktie: :card_file_box: Add User/Buyer of MongoDB

--- crates/database/src/user/model.rs
+++ crates/database/src/user/model.rs
@@ -1,18 +1,11 @@
-use mongodb::bson::oid::ObjectId;
+use chrono::prelude::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 use validator::Validate;
 
-#[derive(Debug, Clone, Serialize, Deserialize, Validate, Default)]
+#[derive(Debug, Clone, Serialize, Deserialize, Validate)]
 pub struct User {
-    #[serde(rename = "_id", skip_deserializing, skip_serializing)]
-    pub id: Option<ObjectId>,
-
-    #[validate(length(min = 1))]
-    pub name: String,
-
-    #[validate(length(min = 1), email(message = "email is invalid"))]
-    pub email: String,
-
-    #[validate(length(min = 6))]
-    pub password: String,
+    pub address: String,          // Address
+    pub amount: String,           // 购买的Hope数量（取整后，存字符串）
+    pub price: String,            // 购买时刻Hope对U的价格（保留20位小数，存字符串）
+    pub timestamp: DateTime<Utc>, // 落库时刻：2024-10-01T04:50:42.849324741Z
 }
--- crates/database/src/user/repository.rs
+++ crates/database/src/user/repository.rs
@@ -1,13 +1,8 @@
-use std::sync::Arc;
-
-use async_trait::async_trait;
-use mongodb::{
-    bson::{doc, oid::ObjectId},
-    results::{DeleteResult, InsertOneResult, UpdateResult},
-};
-use tokio_stream::StreamExt;
-
 use crate::{user::model::User, Database};
+use async_trait::async_trait;
+use chrono::Utc;
+use mongodb::{bson::doc, results::InsertOneResult};
+use std::sync::Arc;
 use utils::AppResult;
 
 pub type DynUserRepository = Arc<dyn UserRepositoryTrait + Send + Sync>;
@@ -17,41 +12,27 @@ pub type DynUserRepository = Arc<dyn UserRepositoryTrait + Send + Sync>;
 pub trait UserRepositoryTrait {
     async fn create_user(
         &self,
-        name: &str,
-        email: &str,
-        password: &str,
+        address: &str,
+        amount: f64,
+        price: f64,
     ) -> AppResult<InsertOneResult>;
 
-    async fn get_user_by_id(&self, id: &str) -> AppResult<Option<User>>;
-
-    async fn get_user_by_email(&self, email: &str) -> AppResult<Option<User>>;
-
-    async fn update_user(
-        &self,
-        id: &str,
-        name: &str,
-        email: &str,
-        password: &str,
-    ) -> AppResult<UpdateResult>;
-
-    async fn delete_user(&self, id: &str) -> AppResult<DeleteResult>;
-
-    async fn get_all_users(&self) -> AppResult<Vec<User>>;
+    async fn get_user(&self, address: &str) -> AppResult<Option<User>>;
 }
 
 #[async_trait]
 impl UserRepositoryTrait for Database {
     async fn create_user(
         &self,
-        name: &str,
-        email: &str,
-        password: &str,
+        address: &str,
+        amount: f64,
+        price: f64,
     ) -> AppResult<InsertOneResult> {
         let new_doc = User {
-            id: None,
-            name: name.to_string(),
-            email: email.to_string(),
-            password: password.to_string(),
+            address: address.to_string(),
+            amount: amount.floor().to_string(),
+            price: format!("{:.20}", price),
+            timestamp: Utc::now(),
         };
 
         let user = self.users.insert_one(new_doc, None).await?;
@@ -59,60 +40,10 @@ impl UserRepositoryTrait for Database {
         Ok(user)
     }
 
-    async fn get_user_by_email(&self, email: &str) -> AppResult<Option<User>> {
-        let filter = doc! {"email": email};
+    async fn get_user(&self, address: &str) -> AppResult<Option<User>> {
+        let filter = doc! {"address": address};
         let user_detail = self.users.find_one(filter, None).await?;
 
         Ok(user_detail)
-    }
-
-    async fn get_user_by_id(&self, id: &str) -> AppResult<Option<User>> {
-        let obj_id = ObjectId::parse_str(id)?;
-        let filter = doc! {"_id": obj_id};
-        let user_detail = self.users.find_one(filter, None).await?;
-
-        Ok(user_detail)
-    }
-
-    async fn update_user(
-        &self,
-        id: &str,
-        name: &str,
-        email: &str,
-        password: &str,
-    ) -> AppResult<UpdateResult> {
-        let id = ObjectId::parse_str(id)?;
-        let filter = doc! {"_id": id};
-        let new_doc = doc! {
-            "$set":
-                {
-                    "name": name,
-                    "email": email,
-                    "password": password,
-                },
-        };
-
-        let updated_doc = self.users.update_one(filter, new_doc, None).await?;
-
-        Ok(updated_doc)
-    }
-
-    async fn delete_user(&self, id: &str) -> AppResult<DeleteResult> {
-        let obj_id = ObjectId::parse_str(id)?;
-        let filter = doc! {"_id": obj_id};
-        let user_detail = self.users.delete_one(filter, None).await?;
-
-        Ok(user_detail)
-    }
-
-    async fn get_all_users(&self) -> AppResult<Vec<User>> {
-        let mut cursor = self.users.find(None, None).await?;
-
-        let mut users: Vec<User> = Vec::new();
-        while let Some(doc) = cursor.next().await {
-            users.push(doc?);
-        }
-
-        Ok(users)
     }
 }
