67d6a07eb65b6a2df012faa64bf9c0cdaa00145f
stevekeol <stevekeol.x@gmail.com>
2025-03-06 14:48:29 +0800

    :necktie: Add user api

--- crates/server/src/api/user_controller.rs
+++ crates/server/src/api/user_controller.rs
@@ -1,36 +1,24 @@
-use axum::{
-    routing::{get, post},
-    Extension, Json, Router,
-};
-
+use crate::services::Services;
+use axum::{extract::Path, routing::get, Extension, Json, Router};
 use database::user::model::User;
-use mongodb::results::InsertOneResult;
-use utils::AppResult;
-
-use crate::{
-    dtos::user_dto::SignUpUserDto, extractors::validation_extractor::ValidationExtractor,
-    services::Services,
-};
+use utils::{AppError, AppResult};
 
 pub struct UserController;
 impl UserController {
     pub fn app() -> Router {
-        Router::new()
-            .route("/", get(Self::all))
-            .route("/signup", post(Self::signup))
+        Router::new().route("/user/:address", get(Self::user))
     }
 
-    pub async fn all(Extension(services): Extension<Services>) -> AppResult<Json<Vec<User>>> {
-        let users = services.user.get_all_users().await?;
-        Ok(Json(users))
-    }
-
-    pub async fn signup(
+    pub async fn user(
         Extension(services): Extension<Services>,
-        ValidationExtractor(req): ValidationExtractor<SignUpUserDto>,
-    ) -> AppResult<Json<InsertOneResult>> {
-        let created_user = services.user.signup_user(req).await?;
-
-        Ok(Json(created_user))
+        Path(address): Path<String>,
+    ) -> AppResult<Json<User>> {
+        match services.user.get_user(address.to_string()).await? {
+            Some(user) => Ok(Json(user)),
+            None => Err(AppError::NotFound(format!(
+                "User with address {} not found.",
+                address
+            ))),
+        }
     }
 }
