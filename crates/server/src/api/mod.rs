fa39778898d5035a98da9da6ff9ac4608be93789
stevekeol <stevekeol.x@gmail.com>
2025-03-06 14:52:51 +0800

    :necktie: Add combine api

--- crates/server/src/api/mod.rs
+++ crates/server/src/api/mod.rs
@@ -1,3 +1,5 @@
+mod refer_controller;
+mod reward_controller;
 mod user_controller;
 
 use axum::routing::{get, Router};
@@ -9,5 +11,7 @@ pub async fn health() -> &'static str {
 pub fn app() -> Router {
     Router::new()
         .route("/", get(health))
-        .nest("/users", user_controller::UserController::app()) // TODO: 统一配置所有路由
+        .nest("/user", user_controller::UserController::app())
+        .nest("/refer", refer_controller::ReferController::app())
+        .nest("/reward", reward_controller::RewardController::app())
 }
