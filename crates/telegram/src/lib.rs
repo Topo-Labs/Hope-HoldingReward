5f021dcdc20317376f91bb2f95dda73528cc0367
stevekeol <stevekeol.x@gmail.com>
2025-03-06 16:58:55 +0800

    :construction: :sparkles: Add Telegram to query, notify, update

---
+++ crates/telegram/src/lib.rs
@@ -0,0 +1,16 @@
+// Telegram: 向用户提供数据查询，消息通知，奖励发放等功能
+
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
